c48f06ae3e0596759b746ee5eea6b856695cc5b0
Keith Benjamin <kjb528i@yahoo.com>
2022-03-07 18:03:44 -0500

    Initial commit

---
+++ src/main/java/com/example/startinfoapi/StartInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.startinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startinfoapi/StartInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
